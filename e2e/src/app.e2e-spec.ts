a9b9ba035aad257bcc84efc9ae090c8dab3c290d
Boryana Dimova <boryana@cloudmybiz.com>
2020-04-24 12:46:25 +0300

    change name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('virtual-academy-courses-angular app is running!');
+    expect(page.getTitleText()).toEqual('virtual-academy app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'virtual-academy-courses-angular'`, () => {
+  it(`should have as title 'virtual-academy'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('virtual-academy-courses-angular');
+    expect(app.title).toEqual('virtual-academy');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('virtual-academy-courses-angular app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('virtual-academy app is running!');
   });
 });
