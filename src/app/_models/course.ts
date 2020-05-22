aee85ba5c910e5eb62d6ae027e98e1e179e4795b
Boryana Dimova <boryana@cloudmybiz.com>
2020-05-22 18:27:25 +0300

    add data

--- src/app/_models/course.ts
+++ src/app/_models/course.ts
@@ -1,6 +1,8 @@
 export interface Course {
+  id?: number;
   title: string;
   description?: string;
   datePublished: Date;
   rating: number;
+  imageUrl?: string;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule} from '@angular/router';
+import { Route, RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './_components/login/login.component';
@@ -8,6 +8,30 @@ import { NavigationComponent } from './_components/navigation/navigation.compone
 import { RegistrationComponent } from './_components/registration/registration.component';
 import { AlertComponent } from './_components/alert/alert.component';
 import { HomeComponent } from './_components/home/home.component';
+import { CoursesComponent } from './_components/courses/courses.component';
+
+const routes: Route[] = [
+  {
+  path: 'home',
+  component: HomeComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegistrationComponent
+  },
+  {
+    path: 'all-courses',
+    component: CoursesComponent
+  },
+  {
+    path: '**',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   declarations: [
@@ -16,10 +40,12 @@ import { HomeComponent } from './_components/home/home.component';
     NavigationComponent,
     RegistrationComponent,
     AlertComponent,
-    HomeComponent
+    HomeComponent,
+    CoursesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
